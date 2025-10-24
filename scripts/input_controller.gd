14e09fdbeca9b1744697ec6f458a2ea6b98f1257
AleD93 <adworld.aled@gmail.com>
2025-10-24 19:17:25 +0900

    Make player moves in camera direction

--- scripts/input_controller.gd
+++ scripts/input_controller.gd
@@ -12,6 +12,7 @@ class_name InputController
 ## Camera is reference for calculating cursor_world_pos
 @export var camera: Camera3D
 @export var cursor_raycast: RayCast3D
+@export var orientation_source: Node3D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -27,6 +28,10 @@ func _process(_delta: float) -> void:
 
 	if is_multiplayer_authority():
 		var new_move_direction: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+
+		if orientation_source:
+			new_move_direction = new_move_direction.rotated(-orientation_source.global_rotation.y)
+
 		if new_move_direction != move_direction:
 			move_direction = new_move_direction
 
