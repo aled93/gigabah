4d765283765a38f94a49780c4ad7f8c9b55ac8d8
AleD93 <adworld.aled@gmail.com>
2025-11-02 13:25:10 +0900

    Add simple parser of command line arguments

--- scripts/app.gd
+++ scripts/app.gd
@@ -1,7 +1,20 @@
+class_name App
 extends Node
+
+static var cmdline_arguments: Dictionary[String, Variant]
 
 @export var dedicated_server_scene: PackedScene
 @export var client_scene: PackedScene
+
+
+static func _static_init() -> void:
+	for arg: String in OS.get_cmdline_args():
+		var parts := arg.split("=", true, 1)
+		if parts.size() == 1:
+			cmdline_arguments.set(parts[0], true)
+			continue
+
+		cmdline_arguments.set(parts[0], parts[1])
 
 
 func _ready() -> void:
