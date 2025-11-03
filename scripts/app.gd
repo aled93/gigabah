07cfadfe307370e60fb723f71ccfe87379df89fd
AleD93 <adworld.aled@gmail.com>
2025-11-03 12:20:58 +0900

    Make app script being autoload and handle game close request to save settings

--- scripts/app.gd
+++ scripts/app.gd
@@ -1,10 +1,9 @@
-class_name App
 extends Node
 
 static var cmdline_arguments: Dictionary[String, Variant]
 
-@export var dedicated_server_scene: PackedScene
-@export var client_scene: PackedScene
+@export var dedicated_server_scene: PackedScene = preload("res://scenes/index.tscn")
+@export var client_scene: PackedScene = preload("res://scenes/ui/main_menu.tscn")
 
 
 static func _static_init() -> void:
@@ -18,6 +17,8 @@ static func _static_init() -> void:
 
 
 func _ready() -> void:
+	get_tree().auto_accept_quit = false
+
 	var next_scene: PackedScene
 
 	if OS.has_feature("dedicated_server"):
@@ -26,4 +27,13 @@ func _ready() -> void:
 	else:
 		next_scene = client_scene
 
+	print("next scene is %s" % next_scene)
 	get_tree().call_deferred(&"change_scene_to_packed", next_scene)
+
+
+func _notification(what: int) -> void:
+	match what:
+		NOTIFICATION_WM_CLOSE_REQUEST:
+			GameSettings.instance.save()
+
+			get_tree().quit()
