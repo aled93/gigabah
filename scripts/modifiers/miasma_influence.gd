ee0442702a53c1662714c5bed8c836360b54431f
AleD93 <adworld.aled@gmail.com>
2025-11-17 21:35:28 +0900

    New ability: inner spirit

---
+++ scripts/modifiers/miasma_influence.gd
@@ -0,0 +1,18 @@
+class_name MiasmaInfluence
+extends Modifier
+
+@export var damage_interval := 0.25
+@export var damage_amount := 1
+
+var _next_tick_time := 0.0
+
+
+func _physics_process(delta: float) -> void:
+	if not carrier:
+		return
+
+	_next_tick_time -= delta
+
+	if _next_tick_time <= 0.0:
+		carrier.health.take_damage(damage_amount)
+		_next_tick_time += damage_interval
