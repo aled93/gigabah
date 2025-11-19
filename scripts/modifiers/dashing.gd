115eba1d2e642a9c62348ad29fb839ecdb116cd9
AleD93 <adworld.aled@gmail.com>
2025-11-19 10:31:53 +0900

    Fix dash stuttering. Disallow move and turn during dashing

--- scripts/modifiers/dashing.gd
+++ scripts/modifiers/dashing.gd
@@ -9,12 +9,21 @@ var _traveled := 0.0
 
 
 func _modifier_start() -> void:
+	modify_property(&"cant_move", true)
+	modify_property(&"cant_turn", true)
+	modify_property(&"no_gravity", true)
+
+	carrier.facing_angle = Vector2(direction.x, direction.z).normalized().angle()
+
 	expire_time = distance / speed
 
 	icon_path = "res://assets/textures/ui/ability_icons/Dash.png"
 
 
 func _physics_process(delta: float) -> void:
+	if not multiplayer.is_server():
+		return
+
 	var step_dist := speed
 
 	carrier.velocity = direction.normalized() * step_dist
